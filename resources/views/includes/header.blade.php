e0fc12a6bcf6fc5eb4c22463216904e503f60244
Unknown <tanmoontaz@gmail.com>
2018-12-05 14:17:36 +0600

    Google Font added
    Alegreya and Lato

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -10,6 +10,9 @@
     <!--<link rel="stylesheet" href="css/fontawesome.min.css">-->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    {{--add font here--}}
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Alegreya" rel="stylesheet">
 
 
     <title>Meal System (beta)</title>
