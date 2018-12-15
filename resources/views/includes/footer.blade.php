de12f927e5488e5a3d356a9723f7f8dd8e05a778
Unknown <tanmoontaz@gmail.com>
2018-12-16 00:04:51 +0600

    usage updated

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -24,6 +24,7 @@
                     <li>Enter new data for the same date and same member will auto overwrite the old data.</li>
                     <li>Every new month will auto create a new meal-system under the meal-manager. Meal-manager can add old member to the new meal-system. Once added, member can not be removed but new month will auto erase all old member from last month.</li>
                     <li>Database only store 2 months data. So user can only see last month's data as past data.</li>
+                    <li>Name and date in table can not be edited. So if you want to change those better delete and enter again.</li>
                     <li>Use <i class="fa fa-user"></i> <i class="fa fa-arrows-h"></i> <i class="fa fa-user-o"></i> this icon to make someone else meal-manager.</li>
                 </ul>
             </div>
